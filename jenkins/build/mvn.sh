f0e6a0a7f2f4f980fa9403538e1e4e60584c9659
root <root@docker-ubuntu.us-west4-b.c.augmented-clock-325512.internal>
2021-10-31 11:02:28 +0000

    fixed

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 echo "******************Buildind jar ++++++++++++++++++"
-WORKSPACE=/var/jenkins_home/workspace/pipeline-docker
+WORKSPACE="/root/jenkins_home/workspace/pipeline-docker"
+echo $WORKSPACE
 docker run --rm -v $WORKSPACE/simple-java-maven-app:/app -v /root/.m2:/root/.m2/ -w /app maven:3-alpine "$@"
--- jenkins/test/mvn.sh
+++ jenkins/test/mvn.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 echo "******************testing++++++++++++++++++"
-WORKSPACE=/var/jenkins_home/workspace/pipeline-docker
+WORKSPACE="/root/jenkins_home/workspace/pipeline-docker"
 docker run --rm -v $WORKSPACE/simple-java-maven-app:/app -v /root/.m2:/root/.m2/ -w /app maven:3-alpine "$@"
